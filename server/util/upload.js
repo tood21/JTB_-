8f3ef257bf1f153ee84592ee93cc9be8bdd4c156
stoneboat <tood2111@gmail.com>
2022-05-10 20:31:37 +0900

    환경변수 재설정

--- server/util/upload.js
+++ server/util/upload.js
@@ -3,16 +3,15 @@ const multer = require("multer");
 const multerS3 = require("multer-s3");
 const endpoint = new AWS.Endpoint("https://kr.object.ncloudstorage.com");
 const region = "kr-standard";
-const access_key = "ZgKXJD6PQrlULnzPJSYi";
-const secret_key = "W7HF75LXmBt4xYpqfTm0GGc1LzagK1COI1ulK62P";
 const path = require("path");
+const config = require("../config/dev.js");
 
 const S3 = new AWS.S3({
   endpoint: endpoint,
   region: region,
   credentials: {
-    accessKeyId: access_key,
-    secretAccessKey: secret_key,
+    accessKeyId: config.access_key,
+    secretAccessKey: config.secret_key,
   },
 });
 
